d8bba3891d1254a8bc7981c5eac764c7fc91d185
kavisho <76555617+kavisho@users.noreply.github.com>
2020-12-25 09:44:32 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,70 @@ const bot     = new Discord.Client({fetchAllMembers: true});
 const fs      = require("fs");
 const moment  = require("moment");
 
+var used1 = false;
+
+const Games = [
+  
+    "Minecraft", 
+    "Grand Theft Auto V", 
+    "Grand Theft Auto : San Andreas", 
+    "Watch Dogs 2"
+];
+
+const statas = [
+'dnd', 
+'idle', 
+'invincible', 
+'online'
+];
 
 
 bot.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+    
+     setInterval(() => {
+
+       const index = Math.floor(Math.random() * (Games.length - 1) + 1);
+       bot.user.setActivity(Games[index]);
+    }, 20000);
+    
+    setInterval(() => {
+
+        const index = Math.floor(Math.random() * (statas.length - 1) + 1);
+        bot.user.setStatus(statas[index]);
+     }, 20000);
+
+
 });
 
-bot.on('message', message => {
-	if (!message.content.startsWith('سلام')) {
-
+bot.on('message', (message) => {
+    
+    if (message.content == "سلام") {
+      
         message.channel.send("سلام <:pogpoker:788071979584454697>")
-    }
+
+    };
+
+    if (message.content == "خوبی؟") {
+      
+        message.channel.send("خوبم<:pogpoker:788071979584454697>")
+
+    };
+
+    if (message.content == "دایر") {
+      
+        message.channel.send("دایر گوز<:pogpoker:788071979584454697>")
+
+    };
+
+
+
+});
+
+bot.on("message", msg => {
+
+    if (msg.author.bot) return;
+    if (msg.channel.type == "dm") return msg.channel.send("<:pogpoker:788071979584454697>");
 });
 
 bot.login(process.env.token);
